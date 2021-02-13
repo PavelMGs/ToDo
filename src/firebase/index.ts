350c59bae2ba003441f721be4056f4ce18721e59
PavelMGs <67228385+PavelMGs@users.noreply.github.com>
2021-02-13 16:29:12 +0200

    Private&Fix

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,28 +1,11 @@
 import firebase from 'firebase/app';
 import 'firebase/database';
 import 'firebase/auth';
+import { firebaseConfig } from '../../private';
 
-
-const firebaseConfig = {
-    apiKey: "AIzaSyCt2p3XFiHRODV2DCyCNaK0g6iB4rrK9-w",
-    authDomain: "todo-2e315.firebaseapp.com",
-    databaseURL: "https://todo-2e315-default-rtdb.firebaseio.com",
-    projectId: "todo-2e315",
-    storageBucket: "todo-2e315.appspot.com",
-    messagingSenderId: "524949307841",
-    appId: "1:524949307841:web:5ccba603b33c2d28a6af40",
-    measurementId: "G-WYCD0PMSBH"
-  };
-
-  export const serviceAccount = require('./todo-2e315-firebase-adminsdk-i7c23-3bef399616.json');
 
 
 firebase.initializeApp(firebaseConfig);
-
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-//   databaseURL: "https://todo-2e315-default-rtdb.firebaseio.com"
-// });
 
 export const database = firebase.database();
 export const auth = firebase.auth()
